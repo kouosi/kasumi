e38230dede5c802aff3eb92ca0a5f41ad406720a
kouosi <kouosi@tuta.io>
2024-08-12 00:31:11 +0545

    feat: I don't even remember what I did so far

--- chat/chat.js
+++ chat/chat.js
@@ -1,7 +1,96 @@
-function convertJSONToHTML(e) {
-    alert("TODO: implement")
+var selected = null;
+
+function constructHtmlOfContactList(data) {
+    const contactList = document.getElementById("contact-list");
+
+    data.contact.forEach(function(data) {
+        const contactInfoDiv = document.createElement("div");
+        contactInfoDiv.className = `contact-info ${data.is_active ? 'online' : 'offline'}`;
+        contactInfoDiv.addEventListener("click", function() {
+            alert("hell");
+        });
+
+        const profileDiv = document.createElement("div");
+        profileDiv.className = "profile";
+
+        const img = document.createElement("img");
+        // img.src = "/avatars/" + data.username;
+        img.src = "https://i.pinimg.com/736x/bc/b6/10/bcb6102adfa68b562a96413551e8ea86.jpg";
+        img.alt = data.name;
+        profileDiv.appendChild(img);
+
+        const infoDiv = document.createElement("div");
+        infoDiv.className = "info";
+
+        const nameDiv = document.createElement("div");
+        nameDiv.className = "name";
+        nameDiv.textContent = data.name;
+        infoDiv.appendChild(nameDiv);
+
+        const lastMessageDiv = document.createElement("div");
+        lastMessageDiv.className = "last-message";
+
+        const textDiv = document.createElement("div");
+        textDiv.className = "text";
+        textDiv.textContent = data.last_message_sent;
+        lastMessageDiv.appendChild(textDiv);
+
+        const timeDiv = document.createElement("div");
+        timeDiv.className = "time";
+        timeDiv.textContent = data.last_seen_time;
+        lastMessageDiv.appendChild(timeDiv);
+
+        infoDiv.appendChild(lastMessageDiv);
+        contactInfoDiv.appendChild(profileDiv);
+        contactInfoDiv.appendChild(infoDiv);
+        contactList.appendChild(contactInfoDiv);
+    })
 }
 
-fetch("/test/chatsample.json").then(e => e.json()).then(e => {
-    convertJSONToHTML(e)
+function constructHtmlOfChatSample(data) {
+    const contactList = document.getElementById("chat-content");
+    const mainchat_window = document.getElementById("chat-title");
+    const username = data.username;
+
+    mainchat_window.className = "chat-title";
+
+    if(data.is_active) {
+        mainchat_window.className += " online";
+    }
+    const userProfileDiv = document.createElement("div");
+    userProfileDiv.className = "profile"
+
+    const userProfileImg = document.createElement("img");
+    userProfileImg.src = "https://i.pinimg.com/736x/bc/b6/10/bcb6102adfa68b562a96413551e8ea86.jpg";
+    userProfileImg.alt = data.username;
+    userProfileDiv.appendChild(userProfileImg)
+
+    const userNameDiv = document.createElement("div");
+    userNameDiv.className = "title";
+    userNameDiv.innerHTML = `${data.name}<div class="last-active">${data.last_online_time}</div>`;
+
+    mainchat_window.appendChild(userProfileDiv);
+    mainchat_window.appendChild(userNameDiv);
+
+    data.message.forEach(function(data) {
+        const contactInfoDiv = document.createElement("div");
+        if (username == data.username) {
+            contactInfoDiv.className = "chat-text chat-right";
+        } else {
+            contactInfoDiv.className = "chat-text chat-left";
+        }
+        contactInfoDiv.innerHTML = data.message;
+        contactList.appendChild(contactInfoDiv);
+    })
+}
+
+const chatContainer = document.getElementById("chat-content");
+chatContainer.scrollTop = chatContainer.scrollHeight;
+
+fetch("/chat/test/contactlist.json").then(e => e.json()).then(e => {
+    constructHtmlOfContactList(e)
+}).catch(e => console.error("Error fetching JSON:", e))
+
+fetch("/chat/test/chatsample.json").then(e => e.json()).then(e => {
+    constructHtmlOfChatSample(e)
 }).catch(e => console.error("Error fetching JSON:", e))
