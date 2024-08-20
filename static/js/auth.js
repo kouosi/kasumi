62520b6e2e6cf316c2a53eaa0f8011aa7660ebb7
kouosi <kouosi@tuta.io>
2024-08-20 11:08:50 +0545

    feat: Add icon in input field

--- static/js/auth.js
+++ static/js/auth.js
@@ -7,22 +7,27 @@ if (login_btn) {
     login_btn.addEventListener('click', function(event) {
         event.preventDefault();
 
-        const email = document.getElementById("form-email").value;
-        const password = document.getElementById("form-password").value;
-        const password_hash = CryptoJS.SHA256(password).toString();
+        const form = document.getElementById("form");
+        if (form.checkValidity()) {
+            const email = document.getElementById("form-email").value;
+            const password = document.getElementById("form-password").value;
+            const password_hash = CryptoJS.SHA256(password).toString();
 
-        fetch('/api/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email: email, password_hash: password_hash })
-        }).then(response => response.json()).then(data => {
-            if (data.success) {
-                window.location.href = "/";
-            } else {
-                errbox.innerHTML = data.message;
-                errbox.style.display = "block";
-            }
-        }).catch(error => console.error('Error:', error));
+            fetch('/api/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email: email, password_hash: password_hash })
+            }).then(response => response.json()).then(data => {
+                if (data.success) {
+                    window.location.href = "/";
+                } else {
+                    errbox.innerHTML = data.message;
+                    errbox.style.display = "block";
+                }
+            }).catch(error => console.error('Error:', error));
+        } else {
+            form.reportValidity();
+        }
     });
 }
 
@@ -30,24 +35,29 @@ if (signup_btn) {
     signup_btn.addEventListener('click', function(event) {
         event.preventDefault();
 
-        const name = document.getElementById("form-name").value;
-        const username = document.getElementById("form-username").value;
-        const email = document.getElementById("form-email").value;
-        const password = document.getElementById("form-password").value;
-        const password_hash = CryptoJS.SHA256(password).toString();
+        const form = document.getElementById("form");
+        if (form.checkValidity()) {
+            const name = document.getElementById("form-name").value;
+            const username = document.getElementById("form-username").value;
+            const email = document.getElementById("form-email").value;
+            const password = document.getElementById("form-password").value;
+            const password_hash = CryptoJS.SHA256(password).toString();
 
-        fetch('/api/signup', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ name: name, email: email, username: username, password_hash: password_hash })
-        }).then(response => response.json()).then(data => {
-            if (data.success) {
-                window.location.href = "/";
-            } else {
-                errbox.innerHTML = data.message;
-                errbox.style.display = "block";
-            }
-        }).catch(error => console.error('Error:', error));
+            fetch('/api/signup', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ name: name, email: email, username: username, password_hash: password_hash })
+            }).then(response => response.json()).then(data => {
+                if (data.success) {
+                    window.location.href = "/";
+                } else {
+                    errbox.innerHTML = data.message;
+                    errbox.style.display = "block";
+                }
+            }).catch(error => console.error('Error:', error));
+        } else {
+            form.reportValidity();
+        }
     });
 }
 
@@ -55,21 +65,26 @@ if (reset_btn) {
     reset_btn.addEventListener('click', function(event) {
         event.preventDefault();
 
-        const email = document.getElementById("form-email").value;
-        const username = document.getElementById("form-username").value;
+        const form = document.getElementById("form");
+        if (form.checkValidity()) {
+            const email = document.getElementById("form-email").value;
+            const username = document.getElementById("form-username").value;
 
-        fetch('/api/reset', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email: email, username: username })
-        }).then(response => response.json()).then(data => {
-            if (data.success) {
-                // Redirect to dashboard or show success message
-            } else {
-                errbox.innerHTML = data.message;
-                errbox.style.display = "block";
-            }
-        }).catch(error => console.error('Error:', error));
+            fetch('/api/reset', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email: email, username: username })
+            }).then(response => response.json()).then(data => {
+                if (data.success) {
+                    // Redirect to dashboard or show success message
+                } else {
+                    errbox.innerHTML = data.message;
+                    errbox.style.display = "block";
+                }
+            }).catch(error => console.error('Error:', error));
+        } else {
+            form.reportValidity();
+        }
     });
 }
 
@@ -77,7 +92,7 @@ const toggle_theme_btn = document.getElementById("toggle-theme");
 function setTheme(isBtn) {
     let current_theme = localStorage.getItem('ui.theme') || 'dark';
     let new_theme = (current_theme === 'dark') ? 'light' : 'dark';
-    if(isBtn) {
+    if (isBtn) {
         document.documentElement.classList.toggle("light-mode", new_theme === 'light');
         localStorage.setItem('ui.theme', new_theme);
     } else {
